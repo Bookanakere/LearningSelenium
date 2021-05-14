ee4e2a83ef5e593f88bdc5f1028649005723b82f
rashmibookanakere <rashmibookanakere@192.168.29.148>
2021-05-14 18:07:50 +0530

    Changed the AUT. Implemented search and click.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,40 +13,20 @@ class Main{
 		
         System.setProperty("webdriver.chrome.driver","chromedriver");
 
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		
+        WebDriver driver = new ChromeDriver(options);
     	
-        String baseUrl = "https://www.phptravels.net/";
+        String baseUrl = "https://desidutchstore.nl/";
 
         driver.get(baseUrl);
         
         WebDriverWait waitTill = new WebDriverWait(driver,10);
 
  		
-        clickElement(By.id("dropdownLangauge"),waitTill);
-        clickElement(By.id("en"),waitTill);
-        clickElement(By.className("select2-input"), waitTill);
-        
-        try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        WebElement destination = driver.findElement(By.className("select2-input"));
-        System.out.println(destination.getAttribute("innerHTML"));
-        destination.sendKeys("Amsterdam");
-        
-  
-		/*
-		 * WebElement languageSwitcher=
-		 * waitTillLanguage.until(ExpectedConditions.elementToBeClickable(By.id(
-		 * "dropdownLangauge"))); languageSwitcher.click();
-		 * 
-		 * WebElement englishLanguage=
-		 * waitTillLanguage.until(ExpectedConditions.elementToBeClickable(By.id("en")));
-		 * englishLanguage.click();
-		 */
+        clickElement(By.className("search-bar__input"),waitTill);
+        clickElement(By.xpath("//ul[contains(@class,'search-bar__menu-linklist')]//a[@href='/collections/snacks']"),waitTill);
  
 	}
 	
