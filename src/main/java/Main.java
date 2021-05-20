9c15f15a5e366f12c499286c4680ef2913dac1ee
rashmibookanakere <rashmibookanakere@192.168.29.148>
2021-05-20 18:14:07 +0530

    Go to cart and check out

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
 
+
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,6 +13,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 class Main{
 
+	
+
+
 	public static void main(String[] args) {
 		
         System.setProperty("webdriver.chrome.driver","chromedriver");
@@ -17,6 +24,9 @@ class Main{
 		options.addArguments("--disable-notifications");
 		
         WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().setScriptTimeout(10,TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
     	
         String baseUrl = "https://desidutchstore.nl/";
 
@@ -30,25 +40,24 @@ class Main{
         clickElement(By.xpath("//ul[contains(@class,'search-bar__menu-linklist')]//a[@href='/collections/snacks']"),waitTill);
         
         
-    
+ 
         
         /* Option 1
         *******Add product directly from main page,  submit form using id *****
         **
-        WebElement submit = driver.findElement(By.id("product_form_id_6040900567190_collection-template"));
-        submit.click();
-        *****Add product directly from main page,  submit form using id ********
-        *
+         clickElement(By.id("product_form_id_6040900567190_collection-template"),waitTill);
+        ****Add product directly from main page,  submit form using id ********
         */
+        
         
         
         
         /* Option 2
          * 
          ********** Add product directly from main page, find form using sibling function *****************/
+        
+         clickElement(By.xpath("//a[@href='/collections/snacks/products/parle-g-big-799g']/following-sibling::div[contains(@class,'product-item__info')]/form[@action='/cart/add']"),waitTill);
          
-         WebElement submit = driver.findElement(By.xpath("//a[@href='/collections/snacks/products/parle-g-big-799g']/following-sibling::div/form[@action='/cart/add']"));
-         submit.click();
        /********** Add product directly from main page, find form using sibling function *****************
        
       
@@ -60,13 +69,28 @@ class Main{
          
         
         clickElement(By.xpath("//a[@href='/collections/snacks/products/parle-g-big-799g']"),waitTill);
-        WebElement submit = driver.findElement(By.xpath("//form[@id='product_form_6040900567190']//div[@class = 'product-form__payment-container']//button[@type='submit']"));
-        submit.click();
+        
+        clickElement(By.xpath("//form[@id='product_form_6040900567190']//div[@class = 'product-form__payment-container']//button[@type='submit']"),waitTill);
         
         /*
          * *****First click on the product, then add to cart, form submission doesn't work here, so find button**********
          */
         
+ 	
+         
+         //click on cart and checkout
+         clickElement(By.xpath("//a[@href='/cart']"),waitTill);
+         clickElement(By.name("checkout"), waitTill);
+         
+         
+         //Enter details
+         
+         //WebElement inputEmail = driver.findElement(By.id("checkout_email_or_phone"));
+         //inputEmail.sendKeys("abc@gmail.com");
+         
+         inputValues(By.id("checkout_email_or_phone"), waitTill, "abc@gmail.com");
+         
+
         try {
 			Thread.sleep(4000);
 		} catch (InterruptedException e) {
@@ -79,13 +103,16 @@ class Main{
 	
 	private static void clickElement(By by, WebDriverWait webDriverWait) { 
 		
-		WebElement returnClickElement= webDriverWait.until(ExpectedConditions.elementToBeClickable(by));        
-			returnClickElement.click();
+		WebElement element= webDriverWait.until(ExpectedConditions.elementToBeClickable(by));        
+			element.click();
 		
 	}
 	
 	
-	
+	private static void inputValues(By by, WebDriverWait webDriverWait, String input) {
+		WebElement element = webDriverWait.until(ExpectedConditions.elementToBeClickable(by));
+		element.sendKeys(input);
+	}
 	
 	/*************** Reference Code *******************************
 	
