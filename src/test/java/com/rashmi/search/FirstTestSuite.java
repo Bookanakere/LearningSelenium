408f923113bc77aa79273b80ff360f81c11950f2
Bookanakere <rash.pb@gmail.com>
2021-05-30 16:41:43 +0530

    created first test suite

---
+++ src/test/java/com/rashmi/search/FirstTestSuite.java
@@ -0,0 +1,11 @@
+package com.rashmi.search;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({SearchTest1.class,SearchTest2.class})
+public class FirstTestSuite {
+
+
+}
